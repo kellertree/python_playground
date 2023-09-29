4df04a858fcc7e9a646011feda137bf5c2322514
Keller Tree <Katree@madisoncollege.edu>
2023-09-29 12:46:31 -0500

    Update playing-with-python.py

--- playing-with-python.py
+++ playing-with-python.py
@@ -1,14 +1,13 @@
-
 # This is a test file.
 # Author: Keller Tree        
 # Date: 9/29/2023
 
-print("Hello World!")
+print("Hello World!\n")
 
-learning_goals = ["programming", "engineering", "science", "data anaytics", "artificial intellgience"]
-print(learning_goals)
+learning_goals = ["programming", "data science", "engineering", "science", "data anaytics", "artificial intellgience", "data engineering"]
+print(f"This a list containing the topics I study. {learning_goals}\n")
 
 for goal in learning_goals:
         print(f"My goal is to learn {goal}.")
 
-print("I have already made considerable progress. It would be even more beneficial if someone would hire me for a related position though.")
+print("\nI have already made considerable progress. It would be even more beneficial if someone would hire me though.")
