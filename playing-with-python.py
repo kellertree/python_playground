6b4373382abdf3e4703672a6deda6901d635f959
Keller Tree <Katree@madisoncollege.edu>
2023-09-29 12:51:00 -0500

    Update playing-with-python.py

--- playing-with-python.py
+++ playing-with-python.py
@@ -7,6 +7,7 @@ print("Hello World!\n")
 learning_goals = ["programming", "data science", "engineering", "science", "data anaytics", "artificial intellgience", "data engineering"]
 print(f"This a list containing the topics I study. {learning_goals}\n")
 
+# The following for loop iterates over each index in the learning_goals list. It then prints out a short message. In short it executes the print statement for each element/index in the list.
 for goal in learning_goals:
         print(f"My goal is to learn {goal}.")
 
